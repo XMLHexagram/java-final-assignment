d12fa5121c60b741d27a1b786e468a3f3a7241d4
lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>
2020-06-13 17:02:27 +0800

    finsh point 1 and 2

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        System.out.printf("test");
+        test tester = new test();
+        tester.output();
     }
 }
 
@@ -12,10 +13,38 @@ class Student {
     private String phone;
     private String ID;
 
+    public Student(String name,double height,double weight,String className,String phone,String ID){
+        this.name = name;
+        this.phone = phone;
+        this.weight = weight;
+        this.height = height;
+        this.className = className;
+        this.ID = ID;
+    }
+
     public boolean isFat() {
         if (this.weight / (Math.pow((this.height / 100), 2)) > 24) {
             return true;
         }
         return false;
+    }
+
+    public void PersonalInfo(){
+        System.out.println(this.name);
+        System.out.println(this.height+"cm");
+        System.out.println(this.weight+"kg");
+        System.out.println(this.className+"班");
+        System.out.println("phone"+this.phone);
+        System.out.println(this.ID);
+    }
+
+
+}
+
+class test {
+    public void output(){
+        Student xiaoming = new Student("xiaoming",180,60,"19050522","18969067887","19052220");
+        xiaoming.PersonalInfo();
+        System.out.println(xiaoming.isFat());
     }
 }
