8a647d7f3fcfca6a65b01c841f80c525600fcfe1
lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>
2020-06-14 12:03:47 +0800

    add sduent score
    Signed-off-by: lmx-Hexagram <52130356+lmx-Hexagram@users.noreply.github.com>

--- src/main.java
+++ src/main.java
@@ -13,17 +13,106 @@ public class main {
 }
 
 class Student {
-    private String name = "test";
-    private double height = 99;
-    private double weight = 99;
-    private String className = "testClass";
-    private String phone = "122251234";
-    private String ID = "134145123";
+    private String name;
+    private double height;
+    private double weight;
+    private String className;
+    private String phone;
+    private String ID;
+    private double English;
+    private double Chinese;
+    private double Math;
+    private double Physics;
+    private double chemical;
+    private double Biology;
+    private double History;
+    private double Computer;
+    private double Sports;
+    private double World;
+
+    public double getEnglish() {
+        return English;
+    }
+
+    public void setEnglish(double english) {
+        English = english;
+    }
+
+    public double getChinese() {
+        return Chinese;
+    }
+
+    public void setChinese(double chinese) {
+        Chinese = chinese;
+    }
+
+    public double getMath() {
+        return Math;
+    }
+
+    public void setMath(double math) {
+        Math = math;
+    }
+
+    public double getPhysics() {
+        return Physics;
+    }
+
+    public void setPhysics(double physics) {
+        Physics = physics;
+    }
+
+    public double getChemical() {
+        return chemical;
+    }
+
+    public void setChemical(double chemical) {
+        this.chemical = chemical;
+    }
+
+    public double getBiology() {
+        return Biology;
+    }
+
+    public void setBiology(double biology) {
+        Biology = biology;
+    }
+
+    public double getHistory() {
+        return History;
+    }
+
+    public void setHistory(double history) {
+        History = history;
+    }
+
+    public double getComputer() {
+        return Computer;
+    }
+
+    public void setComputer(double computer) {
+        Computer = computer;
+    }
+
+    public double getSports() {
+        return Sports;
+    }
+
+    public void setSports(double sports) {
+        Sports = sports;
+    }
+
+    public double getWorld() {
+        return World;
+    }
+
+    public void setWorld(double world) {
+        World = world;
+    }
+
 
     public Student() {
         this.name = randomString();
-        this.height = (int) (Math.random() * 100);
-        this.weight = (int) (Math.random() * 100);
         this.className = randomString();
         this.phone = randomString();
         this.ID = randomString();
@@ -52,7 +141,17 @@ class Student {
                 "班级是: \t" + this.getClassName() + "\n\t" +
                 "身高: \t" + this.getHeight() + "\n\t" +
                 "体重 \t" + this.getWeight() + "\n\t" +
-                "电话号码是: \t" + this.getPhone() + "\n\t";
+                "电话号码是: \t" + this.getPhone() + "\n\t" +
+                "英语成绩 \t" + this.getEnglish() + "\n\t" +
+                "数学成绩 \t" + this.getMath() + "\n\t" +
+                "世界学成绩 \t" + this.getWorld() + "\n\t" +
+                "计算机成绩 \t" + this.getComputer() + "\n\t" +
+                "语文成绩 \t" + this.getChinese() + "\n\t" +
+                "化学成绩 \t" + this.getChemical() + "\n\t" +
+                "物理成绩 \t" + this.getPhysics() + "\n\t" +
+                "体育成绩 \t" + this.getSports() + "\n\t" +
+                "生物成绩 \t" + this.getBiology() + "\n\t" +
+                "历史成绩: \t" + this.getHistory() + "\n\t";
 
 //        return "Student{}";
     }
@@ -116,7 +215,7 @@ class Student {
     }
 
     public boolean isFat() {
-        if (this.weight / (Math.pow((this.height / 100), 2)) > 24) {
+        if (this.weight / ((this.height / 100) * (this.height / 100)) > 24) {
             return true;
         }
         return false;
@@ -153,15 +252,27 @@ class class_ {
         StringBuilder tempPhone = new StringBuilder();
         StringBuilder tempID = new StringBuilder();
 
+        StringBuilder English = new StringBuilder();
+        StringBuilder Chinese = new StringBuilder();
+        StringBuilder Math = new StringBuilder();
+        StringBuilder Physics = new StringBuilder();
+        StringBuilder chemical = new StringBuilder();
+        StringBuilder Biology = new StringBuilder();
+        StringBuilder History = new StringBuilder();
+        StringBuilder Computer = new StringBuilder();
+        StringBuilder Sports = new StringBuilder();
+        StringBuilder World = new StringBuilder();
+
         String json = readToString("store");
         for (int i = 0; i < Objects.requireNonNull(json).length(); i++) {
-            if (json.charAt(i) != '|' && json.charAt(i) != '\n') {
+            if (json.charAt(i) != '|' && json.charAt(i) != '\n' && json.charAt(i) != '\r') {
                 switch (k) {
                     case 1:
                         tempName.append(json.charAt(i));
                         break;
                     case 2:
                         tempHeight.append(json.charAt(i));
+                        System.out.println("test："+tempHeight);
                         break;
                     case 3:
                         tempWeight.append(json.charAt(i));
@@ -175,6 +286,36 @@ class class_ {
                     case 6:
                         tempID.append(json.charAt(i));
                         break;
+                    case 7:
+                        English.append(json.charAt(i));
+                        break;
+                    case 8:
+                        Chinese.append(json.charAt(i));
+                        break;
+                    case 9:
+                        Math.append(json.charAt(i));
+                        break;
+                    case 10:
+                        Physics.append(json.charAt(i));
+                        break;
+                    case 11:
+                        chemical.append(json.charAt(i));
+                        break;
+                    case 12:
+                        Biology.append(json.charAt(i));
+                        break;
+                    case 13:
+                        History.append(json.charAt(i));
+                        break;
+                    case 14:
+                        Computer.append(json.charAt(i));
+                        break;
+                    case 15:
+                        Sports.append(json.charAt(i));
+                        break;
+                    case 16:
+                        World.append(json.charAt(i));
+                        break;
                     default:
                         break;
                 }
@@ -182,14 +323,28 @@ class class_ {
                 k++;
             } else if (json.charAt(i) == '\n') {
                 this.aClass[j] = new Student();
-                System.out.println(tempName);
+//                System.out.println(tempName);
 //                System.out.println(aClass[i]);
+//                System.out.println("aaa:"+tempName);
+//                System.out.println("asdf:"+tempHeight);
                 aClass[j].setName(String.valueOf(tempName));
                 aClass[j].setHeight(Double.parseDouble(String.valueOf(tempHeight)));
                 aClass[j].setWeight(Double.parseDouble(String.valueOf(tempWeight)));
                 aClass[j].setClassName(String.valueOf(tempClassName));
                 aClass[j].setPhone(String.valueOf(tempPhone));
                 aClass[j].setID(String.valueOf(tempID));
+
+
+                aClass[j].setEnglish(Double.parseDouble(String.valueOf(English)));
+                aClass[j].setChinese(Double.parseDouble(String.valueOf(Chinese)));
+                aClass[j].setMath(Double.parseDouble(String.valueOf(Math)));
+                aClass[j].setPhysics(Double.parseDouble(String.valueOf(Physics)));
+                aClass[j].setChemical(Double.parseDouble(String.valueOf(chemical)));
+                aClass[j].setBiology(Double.parseDouble(String.valueOf(Biology)));
+                aClass[j].setHistory(Double.parseDouble(String.valueOf(History)));
+                aClass[j].setComputer(Double.parseDouble(String.valueOf(Computer)));
+                aClass[j].setSports(Double.parseDouble(String.valueOf(Sports)));
+                aClass[j].setWorld(Double.parseDouble(String.valueOf(World)));
 
                 tempName.setLength(0);
                 tempClassName.setLength(0);
@@ -198,13 +353,24 @@ class class_ {
                 tempClassName.setLength(0);
                 tempPhone.setLength(0);
                 tempID.setLength(0);
+
+                English.setLength(0);
+                Chinese.setLength(0);
+                Math.setLength(0);
+                Physics.setLength(0);
+                chemical.setLength(0);
+                Biology.setLength(0);
+                History.setLength(0);
+                Computer.setLength(0);
+                Sports.setLength(0);
+                World.setLength(0);
                 k = 0;
                 j++;
             }
         }
-        System.out.println();
-        System.out.println();
-        System.out.println(aClass[15]);
+//        System.out.println();
+//        System.out.println();
+//        System.out.println(aClass[15]);
 
     }
 
